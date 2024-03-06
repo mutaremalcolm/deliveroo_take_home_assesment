cd7e535c7ff69b2ec81aac47d7c02cb7fb69bfe6
“malcolmmutare” <mutarem@gmail.com>
2024-03-06 22:25:56 +0200

    feat: Add Remote Data Fetching and Logging
    Implemented functionality to fetch data remotely in the project. Data is now retrieved from a remote source, and its contents are logged in the console for debugging and analysis.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
 import Header from './components/Header'
 
+
 function App() {
+  const { menuData, loading, error } = apiDataFetch();
+  console.log("App Component Log:", menuData, loading, error);
+
   return (
     <>
      <Header /> 
