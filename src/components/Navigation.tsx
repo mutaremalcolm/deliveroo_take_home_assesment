2f2ae5ecc5985f0d5fced2139817baa1ac3e0c17
“malcolmmutare” <mutarem@gmail.com>
2024-08-06 12:47:53 +0200

    refactor: Refactor navigation bar for improved UI experience
    - Enhanced the design and structure of the navigation bar for better usability.
    - Streamlined the code for maintainability and clarity.
    - Improved visual aesthetics and responsiveness.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -10,47 +9,50 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 const Navigation = () => {
   return (
     <>
-      <nav className="flex flex-row justify-between items-center pb-2 pr-5 pl-5 md:pb-0">
-        <div className="flex flex-row items-center space-x-2">
-          <Link
-            href="https://github.com/mutaremalcolm/deliveroo_take_home_assesment/tree/main"
-            className="flex items-center space-x-2"
-          >
-            <FaExternalLinkAlt size={15} />
-            <span>Project Source Code</span>
-          </Link>
-        </div>
-        <div className="flex flex-row items-center gap-3">
-          <Link
-            href="https://github.com/mutaremalcolm/technical-take-home_assesment"
-            className="flex items-center text-card-foreground transition-colors hover:text-secondary-foreground"
-          >
-            <FaGithub size={15} />
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/malcolm-mutare-a234a61aa/"
-            className="flex items-center text-card-foreground transition-colors hover:text-secondary-foreground"
-          >
-            <FaLinkedin size={15} />
-          </Link>
-          <Link
-            href="https://github.com/mutaremalcolm/deliveroo_take_home_assesment/tree/main"
-            className="flex items-center text-card-foreground transition-colors hover:text-secondary-foreground"
-          >
-            <FaTwitter size={15} />
-          </Link>
+      <nav className="flex flex-row justify-between pb-2 pr-5 pl-5 md:pb-0">
+        <Link
+          href="https://github.com/mutaremalcolm/deliveroo_take_home_assesment/tree/main"
+          className="flex items-center space-x-2"
+        >
+          <FaExternalLinkAlt size={15} />
+          <span>Project Source Code</span>
+        </Link>
+        <div className="flex flex-col space-y-1 items-center justify-center">
+          <div className="flex flex-row items-center gap-3">
+            <Link
+              href="https://github.com/mutaremalcolm/technical-take-home_assesment"
+              className="flex items-center text-card-foreground transition-colors hover:text-secondary-foreground"
+            >
+              <FaGithub size={15} />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/malcolm-mutare-a234a61aa/"
+              className="flex items-center text-card-foreground transition-colors hover:text-secondary-foreground"
+            >
+              <FaLinkedin size={15} />
+            </Link>
+            <Link
+              href="https://github.com/mutaremalcolm/deliveroo_take_home_assesment/tree/main"
+              className="flex items-center text-card-foreground transition-colors hover:text-secondary-foreground"
+            >
+              <FaTwitter size={15} />
+            </Link>
+          </div>
+          <span className="hidden md:inline-block text-xs font-semibold text-gray-400">
+            by: Malcolm Mutare
+          </span>
         </div>
       </nav>
       <header className="flex flex-col items-center mt-2 border-b border-flipdish-blue md:mx-32">
         <Image
           src="/images/flipdish-logo.svg"
-          height={140}
+          height={150}
           width={75}
           alt="Logo"
-          />
-          <h1>
-            Today's menu
-          </h1>
+        />
+        <h1 className="text-flipdish-blue text-xl md:text-2xl font-bold pt-1 mb-2 md:pt-3 px-10 tracking-widest whitespace-nowrap">
+          Today's menu
+        </h1>
       </header>
     </>
   );
